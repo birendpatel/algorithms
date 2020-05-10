59cdafeeb4e4fac696bb7f20ca3528a766fdbf75
Biren Patel <44389748+birendpatel@users.noreply.github.com>
2020-05-09 23:45:29 -0500

    updated directional fields script
    1. documented methods more clearly
    2. split plot and create_field methods to make more sense. plot no longer handles bounding box and derivative calculations.
    3. added parameters for matplotlib
    4. changed parameters for create_field

--- Differential Equations/directional_field.py
+++ Differential Equations/directional_field.py
@@ -10,15 +10,16 @@ import matplotlib.pyplot as plt
 
 class DirectionalField():
     """
-    Plot a directional field given a suitable differential equation.
+    Plot a directional field given a differential equation. 
 
     Example
     -------
     >>> def differential_equation(t, x):
     >>>     return x*cos(t)
     >>>
-    >>> canvas = DirectionalField(differential_equation)
-    >>> canvas.create_field((0, 10, 20), (-3, 3, 20))
+    >>> DF = DirectionalField(differential_equation)
+    >>> DF.create_field((0, 10, 20), (-3, 3, 20))
+    >>> DF.plot()
     """
 
     def __init__(self, differential_equation):
@@ -26,8 +27,9 @@ class DirectionalField():
         Parameters
         ----------
         differential_equation : function
-            A suitable differential equation with one input variable t and an
-            output variable f(t) = x.
+            A suitable differential equation with input floats t and f(t) = x
+            and an output float representing the derivative. Function must
+            accept arguments in the order of t, x. 
 
         Returns
         -------
@@ -35,80 +37,104 @@ class DirectionalField():
         """
 
         self.differential_equation = differential_equation
-
-    def create_field(self, t_params, x_params):
+        self.domain_image_pairs = []
+        
+    def calculate_derivative(self, t, x):
         """
+        A simple wrapper over the provided differential equation to improve
+        readability of create_field() method and to expose additionality
+        functionality to end user if needed.
+
         Parameters
         ----------
-        t_params : tuple
-            - tuple of (min, max, sample points).
-            - min and max define boundaries of x-axis.
-            - sample points defines number of evenly spaced lines to plot between
-              min and max.
-        x_params : tuple
-            - tuple of (min, max, sample points).
-            - min and max define boundaries of x-axis.
-            - sample points defines number of evenly spaced lines to plot between
-              min and max.
+        t : float
+        x : float
+        
+        Returns
+        -------
+        float
+            The derivative at (t,x) coordinate pair.
+
+        """
+        return self.differential_equation(t, x)
+        
+    def create_field(self, t_parameters, x_parameters, samples):
+        """
+        Calculate derivative at each possible coordinate pair provided via
+        parameters and define the function of a line passing through each
+        coordinate pair with the given slop. Truncate starting and ending
+        coordinates of each line for visualization purposes.
+        
+        Parameters
+        ----------
+        t_parameters : tuple
+            tuple of (min, max) defining plot boundaries of independent axis.
+        x_parameters : tuple
+            tuple of (min, max) defining plot boundaries of dependent axis.
+        samples : int
+            within the rectangular section of space defined by the t and x
+            parameters, a lattice of equally spaced points equal to samples^2
+            will be used to sample the derivative.
 
         Returns
         -------
         None
         """
+        #reset domain-image pairs if method is called more than once
+        self.domain_image_pairs = []
 
-        #set up coordinates
-        t_coords = np.linspace(t_params[0], t_params[1], num=t_params[2])
-        x_coords = np.linspace(x_params[0], x_params[1], num=x_params[2])
-
-        #set up bounding box for the length of each directional line
-        t_box_limits = 0.25  * abs(t_coords[0] - t_coords[1])
-        x_box_limits = 0.25  * abs(x_coords[0] - x_coords[1])
-
-        #plot each directional line
-        self._plot_field(t_coords, x_coords, t_box_limits, x_box_limits)
-
-    def _plot_field(self, t, x, t_box_limits, x_box_limits):
-        """
-        Parameters
-        ----------
-        t : ndarray
-            A sloped line will be plotted at each pair (t,x). t array provides
-            all possible values of t per pair.
-        x : ndarray
-            A sloped line will be plotted at each pair (t,x). x array provides
-            all possible values of x per pair.
-        t_box_limits : float
-            For each plotted line, t_box_limits is used to prevent the line
-            from drawing past the vertical boundaries of a bounding box.
-        x_box_limits : float
-            For each plotted line, x_box_limits is used to prevent the line
-            from drawing past the horizontal boundaries of a bounding box.
-
-        Returns
-        -------
-        None
-        """
-
-        #visualization set up
-        plt.figure(figsize=(15, 15))
-        plt.title('Directional Field', fontsize=25)
-        plt.xlabel('t', fontsize=20)
-        plt.ylabel('f(t) = x', fontsize=20)
+        #the derivative will be calculated for all pairs (t_coords, x_coords)
+        t_coordinates = np.linspace(*t_parameters, samples)
+        x_coordinates = np.linspace(*x_parameters, samples)
+        
+        #an invisible bounding box will be centered over each sampled pair.
+        #We define here the distance of box edges from the center of the box.
+        #these limits prevents the drawns lines from intersecting.
+        t_box_limits = 0.25  * abs(t_coordinates[0] - t_coordinates[1])
+        x_box_limits = 0.25  * abs(x_coordinates[0] - x_coordinates[1])
 
         # loop through each (t,x) and draw a centered line with slope = ddt
-        for i in t:
-            for j in x:
-                #create a line
-                slope = self.differential_equation(i, j)
+        for i in t_coordinates:
+            for j in x_coordinates:
+                #define the equation of a line using the derivative
+                slope = self.calculate_derivative(i, j)
                 domain = np.linspace(i-t_box_limits, i+t_box_limits, num=200)
                 image = slope * (domain - i) + j
 
                 #remove parts of the line that exceed the bounding square box
-                lower_bound = image < j-x_box_limits
-                upper_bound = image > j+x_box_limits
-
+                lower_bound = image < j - x_box_limits
+                upper_bound = image > j + x_box_limits
                 del_inds = np.argwhere((lower_bound)|(upper_bound)).flatten()
                 image = np.delete(image, del_inds)
                 domain = np.delete(domain, del_inds)
+                
+                self.domain_image_pairs.append((domain, image))
 
-                plt.plot(domain, image, color='black')
+    def plot(self, figure_size=(15,15), title_font=25, axis_font=20):
+        """
+        Plot a line at each sampled coordinate pair.
+        
+        Parameters
+        ----------
+        figure_size : tuple, optional
+            matplotlib figure method parameter for figsize. The default is 
+            (15,15).
+        title_font : int, optional
+            matplotlib title method parameter for fontsize. The default is 25.
+        axis_font : int, optional
+            matplotlib xlabel and ylabel method parameter for fontsize. The 
+            default is 20.
+
+        Returns
+        -------
+        None
+        """
+        
+        plt.figure(figsize = figure_size)
+        plt.title("Directional Field", fontsize = title_font)
+        plt.xlabel("t", fontsize = axis_font)
+        plt.ylabel("f(t) = x", fontsize = axis_font)
+
+        #loop through collection of lines and plot each one
+        for i in self.domain_image_pairs:
+            plt.plot(*i, color="black")
