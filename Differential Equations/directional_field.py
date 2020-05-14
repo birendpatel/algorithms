9c706e2e23163a5b2fa0c1b96307d838633f6700
Biren Patel <44389748+birendpatel@users.noreply.github.com>
2020-05-14 13:47:14 -0500

    changed algorithm and delete class
    1. Removed class as I no longer saw the need to retain state anymore
    
    2. Algorithm to create the field was completely changed. I realized that I should use an ellipse instead of a rectangle as the bounding geometric shape that surrounds each line segment. The parametric equation of an ellipse simplifies the problem tremendously because it allows a) an easy method to determine where the line segment endpoints should be and b) a fast, vectorized solution
    
    3. realized (after 4 years of python) that matplotlib can automatically extrapolate a line if you provide only the endpoints. Coupled with the ellipse algorithm, this turned the script from a computational nightmare to a rather fast and memory efficient algorithm.

--- Differential Equations/directional_field.py
+++ Differential Equations/directional_field.py
@@ -1,142 +1,113 @@
 """
 Developer: Biren Patel
-Creation Date: 2020 May 9
-Last Modified: 2020 May 9
-Description: Plot a directional field.
+Description: Plot a directional field for a first-order ODE. Without a stream
+or quiver plot, the algorithm is surprisingly longer than one would initially
+think as it requires the parametric representation of an ellipse.
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-class DirectionalField():
+def DirectionalField(diffeq, x_parameters, y_parameters, samples, \
+                    figure_size=(6,6), title_font=15, axis_font=10):
     """
-    Plot a directional field given a differential equation.
+    Plot a directional field given some first-order ordinary differential
+    equation.
 
-    Example
+    Parameters
+    ----------
+    diffeq : function
+        A first-order ordinary differential equation that must support
+        vectorized operations on 2D numpy arrays. Inputs X and Y are
+        matrices generated from a numpy meshgrid. Output will be a single
+        matrix providing the derivative at each sampled coordinate pair.
+        See functions used in the examples under class docstring.
+
+    t_parameters : tuple
+        tuple of (min, max) defining plot boundaries of independent axis.
+
+    x_parameters : tuple
+        tuple of (min, max) defining plot boundaries of dependent axis.
+
+    samples : int
+        within the rectangular section of space defined by the x and y
+        parameters, a lattice of equally spaced points equal to samples^2
+        will be used to sample the derivative.
+
+    figure_size : tuple, optional
+        matplotlib figure method parameter for figsize. The default is
+        (6,6).
+
+    title_font : int, optional
+        matplotlib title method parameter for fontsize. The default is 15.
+
+    axis_font : int, optional
+        matplotlib xlabel and ylabel method parameter for fontsize. The
+        default is 10.
+
+    Returns
     -------
-    >>> def differential_equation(t, x):
-    >>>     return x*cos(t)
+    None
+
+    Algorithm
+    ---------
+    1. Calculate derivative at each possible coordinate pair provided via
+    the parameters.
+
+    2. For each coordinate pair:
+        2a. Center an ellipse over the coordinate pair.
+
+        2b. Given a line passing through the pair with slope equal to the
+        derivative, find the two coordinate pairs where the line intersects
+        with the ellipse.
+
+        2c. Retain both coordinate pairs as the endpoints of a segment to be
+        plotted.
+
+    Examples
+    --------
+    1.
+    >>> def diffeq(x,y):
+    >>>    return y * np.cos(x)
     >>>
-    >>> DF = DirectionalField(differential_equation)
-    >>> DF.create_field((0, 10), (-3, 3), 20)
-    >>> DF.plot()
+    >>> DirectionalField(diffeq, (-5, 5), (-3,3), 20)
+
+    2.
+    >>> def diffeq(x,y):
+    >>>    return x + y
+    >>>
+    >>> DirectionalField(diffeq, (-10, 10), (-10,10), 20)
     """
+    #sample the x and y coordinates for the requested area in space
+    x_coordinates, x_dist = np.linspace(*x_parameters, samples, retstep=True)
+    y_coordinates, y_dist = np.linspace(*y_parameters, samples, retstep=True)
 
-    def __init__(self, differential_equation):
-        """
-        Parameters
-        ----------
-        differential_equation : function
-            A suitable differential equation with input floats t and f(t) = x
-            and an output float representing the derivative. Function must
-            accept arguments in the order of t, x.
+    #calculate derivative at each sampled coordinate pair
+    X, Y = np.meshgrid(x_coordinates, y_coordinates)
+    ddx = diffeq(X,Y)
 
-        Returns
-        -------
-        None
-        """
+    #width and height of each ellipse, scaled by a factor between 0 and .5
+    width = 0.25 * x_dist
+    height = 0.25 * y_dist
 
-        self.differential_equation = differential_equation
-        self.domain_image_pairs = []
+    #angle of line with x-axis, but use the supplement if angle is negative
+    angle = np.where(ddx < 0, np.arctan(ddx) + np.pi, np.arctan(ddx))
 
-    def calculate_derivative(self, t, x):
-        """
-        A simple wrapper over the provided differential equation to improve
-        readability of create_field() method and to expose additionality
-        functionality to end user if needed.
+    #use the parametric equation of an ellipse to get each coordinate pair
+    x_1 = X + (width * np.cos(angle))
+    y_1 = Y + (height * np.sin(angle))
+    x_2 = X - (width * np.cos(angle))
+    y_2 = Y - (height * np.sin(angle))
 
-        Parameters
-        ----------
-        t : float
-        x : float
+    #set up plot details
+    plt.figure(figsize = figure_size)
+    plt.title("Directional Field", fontsize = title_font)
+    plt.xlabel("x", fontsize = axis_font)
+    plt.ylabel("f(x) = y", fontsize = axis_font)
 
-        Returns
-        -------
-        float
-            The derivative at (t,x) coordinate pair.
+    #graph each segment of a line onto the plot
+    for i in range(samples):
+        for j in range(samples):
+            plt.plot([x_1[i][j], x_2[i][j]], [y_1[i][j], y_2[i][j]], color="black")
 
-        """
-        return self.differential_equation(t, x)
-
-    def create_field(self, t_parameters, x_parameters, samples):
-        """
-        Calculate derivative at each possible coordinate pair provided via
-        parameters and define the function of a line passing through each
-        coordinate pair with the given slop. Truncate starting and ending
-        coordinates of each line for visualization purposes.
-
-        Parameters
-        ----------
-        t_parameters : tuple
-            tuple of (min, max) defining plot boundaries of independent axis.
-        x_parameters : tuple
-            tuple of (min, max) defining plot boundaries of dependent axis.
-        samples : int
-            within the rectangular section of space defined by the t and x
-            parameters, a lattice of equally spaced points equal to samples^2
-            will be used to sample the derivative.
-
-        Returns
-        -------
-        None
-        """
-        #reset domain-image pairs if method is called more than once
-        self.domain_image_pairs = []
-
-        #the derivative will be calculated for all pairs (t_coords, x_coords)
-        t_coordinates, t_dist = np.linspace(*t_parameters, samples, retstep=True)
-        x_coordinates, x_dist = np.linspace(*x_parameters, samples, retstep=True)
-
-        #an invisible bounding box will be centered over each sampled pair.
-        #We define here the distance of box edges from the center of the box.
-        #these limits prevents the drawns lines from intersecting.
-        t_box_limits = 0.25  * t_dist
-        x_box_limits = 0.25  * x_dist
-
-        # loop through each (t,x) and draw a centered line with slope = ddt
-        for i in t_coordinates:
-            for j in x_coordinates:
-                #define the equation of a line using the derivative
-                slope = self.calculate_derivative(i, j)
-                domain = np.linspace(i-t_box_limits, i+t_box_limits, num=200)
-                image = slope * (domain - i) + j
-
-                #remove parts of the line that exceed the bounding square box
-                lower_bound = image < j - x_box_limits
-                upper_bound = image > j + x_box_limits
-                del_inds = np.argwhere((lower_bound)|(upper_bound))
-                image = np.delete(image, del_inds)
-                domain = np.delete(domain, del_inds)
-
-                self.domain_image_pairs.append((domain, image))
-
-    def plot(self, figure_size=(6,6), title_font=15, axis_font=10):
-        """
-        Plot a line at each sampled coordinate pair.
-
-        Parameters
-        ----------
-        figure_size : tuple, optional
-            matplotlib figure method parameter for figsize. The default is
-            (6,6).
-        title_font : int, optional
-            matplotlib title method parameter for fontsize. The default is 15.
-        axis_font : int, optional
-            matplotlib xlabel and ylabel method parameter for fontsize. The
-            default is 10.
-
-        Returns
-        -------
-        None
-        """
-
-        plt.figure(figsize = figure_size)
-        plt.title("Directional Field", fontsize = title_font)
-        plt.xlabel("t", fontsize = axis_font)
-        plt.ylabel("f(t) = x", fontsize = axis_font)
-
-        #loop through collection of lines and plot each one
-        for i in self.domain_image_pairs:
-            plt.plot(*i, color="black")
-
-        plt.show()
+    plt.show()
