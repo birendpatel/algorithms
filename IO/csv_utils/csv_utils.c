4717b87e7f2cb3a0b6506d608ad5af0aedf65227
Biren Patel <44389748+birendpatel@users.noreply.github.com>
2020-06-12 00:57:24 -0500

    finished separator function

--- IO/csv_utils/csv_utils.c
+++ IO/csv_utils/csv_utils.c
@@ -1,6 +1,8 @@
 /*
 * author: Biren Patel
-* description: command line program for CSV utilities
+* description: command line program for CSV utilities, quick and dirty CSV file
+* cleanup operations for files that can risk unsafe overwrites. emphasis on
+* unsafe - I'm just trying to do my C work faster here!
 */
 
 #include <stdio.h>
@@ -10,6 +12,10 @@
 #include <stdbool.h>
 
 #define BUFFER_LEN 2048
+#define TEMP_FILENAME "_csv_utils_tempfile_.csv"
+
+FILE *csvfile;
+char *filename;
 
 /*******************************************************************************
 * function prototypes
@@ -17,48 +23,48 @@
 
 void display_instructions(void);
 int get_user_code(void);
-
-bool csv_replace_sep(FILE *csvfile, FILE *newfile);
-bool csv_add_missing_commas_tail(FILE *csvfile, FILE *newfile);
+void swap_files(void);
+void csv_replace_sep(void);
+void csv_add_missing_commas_tail(void);
 
 /******************************************************************************/
 
 int main(int argc, char **argv)
 {
     //open file with supplied user argument
-    FILE *csvfile = fopen(argv[1], "r");
+    filename = argv[1];
+    csvfile = fopen(filename, "r");
 
-    //open file to place new contents
-    FILE *newfile = fopen(argv[2], "w");
-
-    if (csvfile == NULL || newfile == NULL)
+    if (csvfile == NULL)
     {
         printf("Ensure file exists at location.\n");
-        printf("Usage: csv_utils <filename> <filename>");
+        printf("Usage: csv_utils <filename>");
         exit(EXIT_FAILURE);
     }
+
+    display_instructions();
 
     //main program loop
     while(true)
     {
-        display_instructions();
+        printf("\n\n>>> Enter an instruction code: [ ]\b\b");
 
         switch(get_user_code())
         {
             case 1:
                 //replace separator
-                csv_replace_sep(csvfile, newfile);
+                csv_replace_sep();
+                printf(">>> requested changes are complete.");
                 break;
 
             case 2:
                 //add missing commas
-                csv_add_missing_commas_tail(csvfile, newfile);
+                csv_add_missing_commas_tail();
                 break;
 
             default:
-                printf("Program terminated\n");
+                printf(">>> Program terminated\n");
                 fclose(csvfile);
-                fclose(newfile);
                 return EXIT_SUCCESS;
         }
     }
@@ -70,6 +76,7 @@ int main(int argc, char **argv)
 
 void display_instructions(void)
 {
+    system("cls");
     printf("\n\n     CSV Utilities - Biren Patel - " __DATE__" - "__TIME__);
     printf("\n|-----------------------------------------------------------|");
     printf("\n\n\t\tCodes\tPurpose\n\t\t-----\t-----------------\n");
@@ -77,8 +84,6 @@ void display_instructions(void)
     printf("\t\t  1  \treplace separator\n");
     printf("\t\t  2  \tadd missing commas\n");
     printf("\n|-----------------------------------------------------------|");
-
-    printf("\n\nEnter an instruction code: ");
 }
 
 /******************************************************************************/
@@ -91,24 +96,43 @@ int get_user_code(void)
     return user_code;
 }
 
+/******************************************************************************/
+
+void swap_files(void)
+{
+    fclose(csvfile);
+    remove(filename);
+    rename(TEMP_FILENAME, filename);
+    csvfile = fopen(filename, "r");
+
+    assert(csvfile != NULL);
+}
+
 /*******************************************************************************
 * function: csv_replace_sep
 * purpose: replace all instances of one separator with a different separator
 * @ csvfile : pointer to file object
 * returns : true if successful, false otherwise
 *******************************************************************************/
-bool csv_replace_sep(FILE *csvfile, FILE *newfile)
+void csv_replace_sep(void)
 {
+    FILE *newfile = fopen(TEMP_FILENAME, "w");
+
     char old, new;
     char buffer[BUFFER_LEN];
 
-    printf("Enter the current separator: ");
+    printf(">>> Enter the current separator: [ ]\b\b");
     old = getchar();
     while(getchar() != '\n');
 
-    printf("Enter the replacement separator: ");
+    printf(">>> Enter the replacement separator: [ ]\b\b");
     new = getchar();
     while(getchar() != '\n');
+
+    if (new == old)
+    {
+        return;
+    }
 
     while(fgets(buffer, BUFFER_LEN, csvfile))
     {
@@ -120,7 +144,8 @@ bool csv_replace_sep(FILE *csvfile, FILE *newfile)
         fputs(buffer, newfile);
     }
 
-    return true;
+    fclose(newfile);
+    swap_files();
 }
 
 /*******************************************************************************
@@ -129,7 +154,7 @@ bool csv_replace_sep(FILE *csvfile, FILE *newfile)
 * @ csvfile : pointer to file object
 * returns : true if successful, false otherwise
 *******************************************************************************/
-bool csv_add_missing_commas_tail(FILE *csvfile, FILE *newfile)
+void csv_add_missing_commas_tail(void)
 {
-    return true;
+
 }
