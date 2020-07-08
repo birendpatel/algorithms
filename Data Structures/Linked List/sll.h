30c8cfd352118b92188a8bd8e9857d60920f8cda
Biren Patel <44389748+birendpatel@users.noreply.github.com>
2020-07-08 12:38:53 -0500

    finished preliminary header
    enough to get started on the implementation

--- Data Structures/Linked List/sll.h
+++ Data Structures/Linked List/sll.h
@@ -16,9 +16,9 @@ typedef int list_item;
 
 /*******************************************************************************
 * structure: node
+* purpose: describes a single node within the list
 * @ datum : the item stored at this node
 * @ next : pointer to the next node in the list
-* purpose: describes a single node within the list
 *******************************************************************************/
 struct node
 {
@@ -28,9 +28,9 @@ struct node
 
 /*******************************************************************************
 * structure: sll
+* purpose: client must declare pointer to this struct to access the API
 * @ destroy : pointer to function used to clean up memory on destructor call
 * @ head : pointer to the first node in the list
-* purpose: client must declare variable of type sll to access API
 *
 *
 *       SLL  
@@ -47,5 +47,32 @@ struct sll
     struct node *head;
     uint32_t size;    
 };
+
+/*******************************************************************************
+* public function: sll_create
+* purpose: constructor
+* @ destroy : pointer to function used to clean up memory on destructor call
+* returns: pointer to struct sll
+*******************************************************************************/
+struct sll *sll_create(void (*destroy)(void *object));
+
+
+/*******************************************************************************
+* public function: sll_destroy
+* purpose: destructor
+* @ s : pointer to struct sll
+*******************************************************************************/
+void sll_destroy(struct sll *s);
+
+
+/*******************************************************************************
+* public function: sll_insert_idx
+* purpose: insert a node at the specified index
+* @ s : pointer to struct sll
+* @ idx : index of insertion, where 0 is the head
+* @ datum : list item to store at the new node
+* returns: pointer to newly created struct node, or NULL on failure
+*******************************************************************************/
+struct node *sll_insert_idx(struct sll *s, uint32_t idx, list_item datum);
 
 #endif
