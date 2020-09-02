e0e54b67d4527e85359e39179709f1102b6077d6
Biren Patel <44389748+birendpatel@users.noreply.github.com>
2020-09-02 14:23:06 -0500

    pmalloc returns aligned pointer

--- Memory/mempool.c
+++ Memory/mempool.c
@@ -6,13 +6,47 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <assert.h>
 
 #include "mempool.h"
 
 /*******************************************************************************
 * struct: block
 * purpose: doubly linked list node hidden before requested memory blocks
-* note: in total, if a user requests an x-byte block, x + 32 bytes are reserved
+* @ size : the byte-size of the memory block handed back to the user
+* @ available : flag where 1 indiciates that the block is free
+* @ top_gap : bytes between the start of this block and end of the last block
+* note: in total, if a user requests an x-byte block, x + top_gap + 32 bytes
+*       are reserved, but only x + 32 bytes are actually used.
+*
+*
+* struct: manager
+* purpose: doubly linked list and memory pool manager
+* @ top : points to first open and aligned byte after the last occupied block
+* @ max_free : block.size of the largest available block
+* @ available : total bytes available outside of blocks (from *top onward)
+*
+*
+*
+*  pool start                                    top pointer
+*     |                                               |
+*     |                                               |
+*     V                                               V
+*
+*     #-------------------#-----#--------------------#------------------------#
+*     # block + user data #     # block + user data  #                        #
+*     #-------------------#-----#--------------------#------------------------#
+*    
+*     \__________________/\_____/                     \______________________/
+*              |             \                                available
+*              |              \
+*              |               \_________ gap between blocks
+*              |                          to ensure alignment
+*   block may be free, the size 
+*   of the largest free block
+*   is stored in max_free member
+*   
 *******************************************************************************/
 
 #define SIZEOF_BLOCK 32
@@ -23,36 +57,10 @@ struct block
     struct block *next;
     size_t size;   
     char available;
-    char reserved[7];
+    char top_gap;
+    char reserved[6];
 };
 
-/*******************************************************************************
-* struct: manager
-* purpose: doubly linked list and memory pool manager
-* @ top : points to the first available byte after the last occupied block
-* @ max_free : block.size of the largest available block
-* @ available : total bytes available outside of blocks (from *top onward)
-*
-*
-*
-*  pool start                              top pointer
-*     |                                        |
-*     |                                        |
-*     V                                        V
-*
-*     #-------------------#-------------------#-------------------------------#
-*     # block + user data # block + user data #                               #
-*     #-------------------#-------------------#-------------------------------#
-*    
-*     \__________________/                     \_____________________________/
-*              |                                           available
-*              |
-*              |
-*   block may be free, the size 
-*   of the largest free block
-*   is stored in max_free member
-*   
-*******************************************************************************/
 
 static struct
 {
@@ -74,9 +82,14 @@ manager =
 };
 
 /******************************************************************************/
-//prototypes
+//prototypes and general macros
 
-static void insert_node(struct block *new);
+//using 8-byte alignment to handle most types.
+//0x7 instead of 0x8 b/c alignment is just used to round down manager.top
+#define ALIGN ((uintptr_t) 0x7)
+#define ALIGN_MASK (~ ((uintptr_t) 0x7))
+
+static void insert_node_at_tail(struct block *new);
 
 /******************************************************************************/
 
@@ -134,26 +147,37 @@ void *pmalloc(size_t size)
 {
     if (size == 0) return NULL;
     
-    //repurpose an available block else use a new section of the pool
+    //try to repurpose an available block
     if (size <= manager.max_free)
     {
         return NULL;
     }
-    else if (size <= manager.available - 32)
+    //otherwise use a new section of the pool
+    else if (size + ALIGN + SIZEOF_BLOCK <= manager.available)
     {
-        //place a new list node at the top of the pool
+        //align top pointer to somewhere between old top and ALIGN-bytes above
+        uintptr_t old_top = (uintptr_t) manager.top;
+        manager.top = (void*) (((uintptr_t) manager.top + ALIGN) & ALIGN_MASK);
+        
+        char delta = (char) ((uintptr_t) manager.top - old_top);
+        assert((delta >= 0 || delta <= ALIGN) && "delta exceeds allowed gap");
+        
+        //place a new block node at top and add to tail of linked list
         struct block *new = manager.top;
+        
         new->size = size;
         new->available = false;
+        new->top_gap = delta;
+        insert_node_at_tail(new);
         
-        //add node references
-        insert_node(new);
-        
-        //update manager pool metadata
-        manager.available -= SIZEOF_BLOCK + size;
+        //update manager, move top a la sbrk() and note total bytes used
+        manager.available -= SIZEOF_BLOCK + size + delta;
         manager.top = (char*) manager.top + (SIZEOF_BLOCK + size);
         
         //hide the metadata and return 32 bytes above for user to use
+        assert((uintptr_t) new % 8 == 0 && "struct block not aligned");
+        assert((uintptr_t) (new + 1) % 8 == 0 && "user block not aligned");
+        
         return new + 1;
     }
     else
@@ -167,6 +191,7 @@ void *pmalloc(size_t size)
 
 void *pcalloc(size_t n, size_t size)
 {
+    //todo: check for overflow before passing
     void *address = pmalloc(n * size);
     
     if (address == NULL)
@@ -187,23 +212,22 @@ void pfree(void *ptr)
     if (ptr == NULL) return;
     
     //container of ptr
-    struct block *meta = (char*) ptr - SIZEOF_BLOCK;
+    struct block *meta = (struct block *) ((char*) ptr - SIZEOF_BLOCK);
     
     //psuedo-free, don't return to system just mark the block for reuse
     meta->available = 1;
     
+    //todo: join neighbor blocks
+    //1. try to combine this with previous block
+    //2. try to combine this potentially new block with the next block
+    
     //notify manager
     if (manager.max_free < meta->size) manager.max_free = meta->size;
-    
-    //could do some funky stuff here
-    //try to join with neighbor blocks if they are free
-    //or send the node to the head for faster walk on next malloc
 }
 
 /******************************************************************************/
-//insert a block node into the tail of the linked list
 
-static void insert_node(struct block *new)
+static void insert_node_at_tail(struct block *new)
 {
     if (manager.head == NULL)
     {
@@ -222,13 +246,14 @@ static void insert_node(struct block *new)
         manager.tail = new;
     }
 }
+
 /******************************************************************************/
 
 #include <stdio.h>
 
 int main(void)
 {
-    mempool_init(100);
+    mempool_init(1000);
     
     pmalloc(18);
     
